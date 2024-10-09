02d480ad353b3bc4a9bc0f04908f9e675debdcd3
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-09 15:34:54 +0500

    Task4

---
+++ Tyuiu.SilinEV.Sprint2.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.SilinEV.Sprint2.Task4.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint2.Task4.V16/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tyuiu.SilinEV.Sprint2.Task4.V16.Lib;
+
+double q = Convert.ToDouble(Console.ReadLine());
+double w = Convert.ToDouble(Console.ReadLine());
+DataService ds = new();
+Console.WriteLine(ds.Calculate(q, w));
