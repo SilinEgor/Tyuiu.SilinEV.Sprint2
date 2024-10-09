c2f92a0c2c7d5cb03a8453e9299603cca5488cb2
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-09 16:01:09 +0500

    Task5

--- Tyuiu.SilinEV.Sprint2.Task5.V8/Program.cs
+++ Tyuiu.SilinEV.Sprint2.Task5.V8/Program.cs
@@ -2,4 +2,4 @@
 
 DataService ds = new();
 int q = Convert.ToInt32(Console.ReadLine()), w = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(ds.FindDateOfPreviousDay(q, w));
+Console.WriteLine(ds.FindDateOfPreviousDay(w, q));
