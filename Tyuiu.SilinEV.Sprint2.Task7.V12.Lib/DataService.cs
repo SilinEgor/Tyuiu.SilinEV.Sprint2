79f45671803d9bd932b54804921858d3500c15fc
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-09 16:45:31 +0500

    Task7

---
+++ Tyuiu.SilinEV.Sprint2.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.SilinEV.Sprint2.Task7.V12.Lib
+{
+    public class DataService : ISprint2Task7V12
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            if (x < 0)
+            {
+                return ((2 - x < y) && (y < x * x)) ? true : false;
+            }
+            else
+            {
+                return ((y > 0) && (y < 2 - x) && (y < x * x)) ? true : false;
+            }
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint2.Task7.V12/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tyuiu.SilinEV.Sprint2.Task7.V12.Lib;
+
+DataService ds = new();
+double x = Convert.ToDouble(Console.ReadLine()), y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(ds.CheckDotInShadedArea(x, y));
