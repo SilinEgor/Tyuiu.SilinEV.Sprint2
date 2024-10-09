6c4f742ff76f2ae00f0894898237de18757478a6
Tyuiu.SilinEV@outlook.com <SilinEgor369@gmail.com>
2024-10-09 16:00:31 +0500

    Task5

---
+++ Tyuiu.SilinEV.Sprint2.Task5.V8.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.SilinEV.Sprint2.Task5.V8.Lib
+{
+    public class DataService : ISprint2Task5V8
+    {
+        public string FindDateOfPreviousDay(int m, int n)
+        {
+            switch (n)
+            {
+                case 1:
+                    switch (m)
+                    {
+                        case 3:
+                            n = 28;
+                            break;
+                        case 2:
+                        case 4:
+                        case 6:
+                        case 8:
+                        case 10:
+                        case 12:
+                            n = 31;
+                            break;
+                        default:
+                            n = 30;
+                            break;
+                    }
+
+                    m -= 1;
+                    break;
+            }
+            string a = Convert.ToString(n).Length == 2 ? Convert.ToString(n) : "0" + n;
+            string s = Convert.ToString(m).Length == 2 ? Convert.ToString(m) : "0" + m;
+            return a + "." + s;
+        }
+    }
+}
---
+++ Tyuiu.SilinEV.Sprint2.Task5.V8/Program.cs
@@ -0,0 +1,5 @@
+﻿using Tyuiu.SilinEV.Sprint2.Task5.V8.Lib;
+
+DataService ds = new();
+int q = Convert.ToInt32(Console.ReadLine()), w = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ds.FindDateOfPreviousDay(q, w));
